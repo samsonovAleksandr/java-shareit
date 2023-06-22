5bf34365e1be1ffe00a3abff9f4d5d32f563c7b9
Alexander Samsonov <samsonov110294@yandex.ru>
2023-06-22 11:14:24 +0500

    реализовано сохранение booking и comment

--- src/test/java/ru/practicum/shareit/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/UserControllerTest.java
@@ -23,8 +23,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class UserControllerTest {
-
-    private final MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
     private final ObjectMapper objectMapper;
 
